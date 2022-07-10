e697325cf8a6d07026a928c2546bd9492263840c
shakedizzy <shakedizzy81@gmail.com>
2022-07-10 16:45:48 +0400

    Add GCD game logic

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,26 +1,20 @@
-import operator
-from random import choice, randint
+from random import randint
 
 
-allowed_operators = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul
-}
+def calculate(number_1, number_2):
+    min_number = min(number_1, number_2)
+    max_number = max(number_1, number_2)
+    if max_number % min_number == 0:
+        return min_number
 
-
-def calculate_exp():
-
-    operand_1 = randint(1, 50)
-    operand_2 = randint(1, 50)
-    operation = choice(list(allowed_operators.keys()))
-
-    expr = str(operand_1) + ' ' + operation + ' ' + str(operand_2)
-    print(expr)
-    correct_answer = str(allowed_operators[operation](operand_1, operand_2))
-    return expr, correct_answer
+    for i in range(min_number // 2, 0, -1):
+        if max_number % i == 0 and min_number % i == 0:
+            return i
 
 
 def main():
-    result = calculate_exp()
-    return result
+    number_1 = randint(1, 100)
+    number_2 = randint(1, 100)
+    question = str(number_1) + ' ' + str(number_2)
+    correct_answer = str(calculate(number_1, number_2))
+    return question, correct_answer
