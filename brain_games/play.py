46f88c1d3535e20a631fb84ea5c27b46707e4efd
shakedizzy <shakedizzy81@gmail.com>
2022-07-11 08:49:37 +0400

    Change quotation in output

--- brain_games/play.py
+++ brain_games/play.py
@@ -30,8 +30,8 @@ def play_games(game, username):
             print('Correct!')
             continue
         else:
-            print(f'"{answer}" is wrong answer ;(. \
-Correct answer was "{correct_answer}".')
+            print(f"'{answer}' is wrong answer ;(. \
+Correct answer was '{correct_answer}'.")
             print(f'Let\'s try again, {username}!')
             return
 
